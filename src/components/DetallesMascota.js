f7f4b5fa932c42a747d65dced2e3f82f3fd8d6cc
alvaroreinoso <alvaroreinoso@alu.frt.utn.edu.ar>
2022-08-30 23:25:57 -0300

    asd

--- src/components/DetallesMascota.js
+++ src/components/DetallesMascota.js
@@ -4,12 +4,12 @@ import data from "./data"
 
 import {Link} from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faHouse, faList, faPhone, faSearch } from '@fortawesome/free-solid-svg-icons'
-
+import { faCoffee, faEnvelope, faEnvelopeOpen, faHouse, faList, faPhone, faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
+import imagenAnimal from "../images/${thisService.foto1}"
 
 const phoneIcon = <FontAwesomeIcon icon={faPhone} />
-const searchIcon = <FontAwesomeIcon icon={faSearch} />
-const listhIcon = <FontAwesomeIcon icon={faList} />
+const userIcon = <FontAwesomeIcon icon={faUser} />
+const mailIcon = <FontAwesomeIcon icon={faEnvelope} />
 
 
 function DetallesMascota(props) {
@@ -23,40 +23,86 @@ function DetallesMascota(props) {
     return (
         <div class="detalles--mascota">
 
+
+        <div class="campos-total">
+        <label>FOTO</label>
+        <br />
+        <img src={thisService.foto1}/>
+        
+       <div class="campos-derecha">
+
+       </div>
+
+
+        <div class="campos-principal">
+
+        <div class="campos">
+        <label>Nombre</label>
+        <br />
+        <label>Raza</label>
+        <br />
+        <label>Sexo</label>
+<br />
+        <label>Años</label>
+<br />
+        <label>Chip</label>
+<br />
+        <label>Orejas</label>
+<br />
+        <label>Color</label>
+<br />
+        <label>Observaciones</label>
+        </div>
+
+
+        <div class="campos-datos">
+        <label>{thisService.nombre}</label>
+        <br />
+       <label>{thisService.raza}</label>
+       <br />
+            <label>{thisService.sexo}</label>
+<br />
+            <label>{thisService.años}</label>
+<br />
+            <label>{thisService.chip}</label>
+<br />
+            <label>{thisService.orejas}</label>
+<br />
+            <label>{thisService.color}</label>
+    <br />        
+            <label class="observaciones">{thisService.observaciones}</label>
+        </div>
+       
+        </div>
         
 
-        <label>Nombre</label>
-        <label class="box_label">{thisService.nombre}</label>
-        <br/>
-        <label>Raza</label>
-        <label class="box_label">{thisService.raza}</label>
-        <br/>
-         <label>Sexo</label>
-         <label class="box_label">{thisService.sexo}</label>
-        <br/>
-        <label>Años</label>
-        <label class="box_label">{thisService.años}</label>
-        <br/>
-        <label>Chip</label>
-        <label class="box_label">{thisService.chip}</label>
-        <br/>
-        <label>Orejas</label>
-        <label class="box_label">{thisService.orejas}</label>
-        <br/>
-        <label>Color</label>
-        <label class="box_label">{thisService.color}</label>
-        <br/>
-        <label>Observaciones</label><br />
-        <label class="box_label">{thisService.observaciones}</label>
-        <br/>
+
+        </div>
+
+        <h3 id="datos-de-contacto">DATOS DE CONTACTO</h3>
+        <div class="datos-contacto-dueño">
         
-        <label class="box_dueño">{phoneIcon} <br /> NOMBRE <br />{thisService.nombreDueño}</label>
-        <br/>
-        <label>TELEFONO</label>
-        <label>{thisService.telefonoDueño}</label>
-        <br/>
-        <label>MAIL</label>
-        <label>{thisService.mailDueño}</label>
+        <div class="box_dueño_nombre">
+        <p id="box_dueño-user">{userIcon}</p>
+        <p id="box_dueño-nombre-1">DUEÑO</p>
+        <p id="box_dueño-nombre">{thisService.nombreDueño}</p>
+        </div>
+
+
+        <div class="box_dueño_telefono">
+        <p id="box_dueño-user">{phoneIcon}</p>
+        <p id="box_dueño-nombre-1">TELEFONO</p>
+        <p id="box_dueño-nombre">{thisService.telefonoDueño}</p>
+        </div>
+        
+        <div class="box_dueño_correo">
+        <p id="box_dueño-user">{mailIcon}</p>
+        <p id="box_dueño-nombre-1">CORREO</p>
+        <p id="box_dueño-nombre">{thisService.mailDueño}</p>
+        </div>
+       
+        </div>
+      
 
 
 
